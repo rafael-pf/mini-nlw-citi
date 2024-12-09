9dc331dea456f117de35a8c538ac3a541c815e5e
Rafael <rafapazfernandes@gmail.com>
2024-12-09 14:25:03 -0300

    feat(server): cors added

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,12 +1,14 @@
 import routes from "./routes";
 import dotenv from "dotenv";
 import express from "express";
+import cors from "cors";
 import "@database";
 
 dotenv.config();
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 app.use(routes);
 app.use(express.static(__dirname + "/public"));
 
