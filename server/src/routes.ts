5a568ce0961ede5d84099ff7d9114d85b097b515
Rafael <rafapazfernandes@gmail.com>
2024-12-09 18:07:48 -0300

    feat(server): added task routes

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import userController from "./controllers/UserController";
+import taskController from "./controllers/TaskController";
 
 const routes = express.Router();
 
@@ -7,5 +8,14 @@ routes.post("/user", userController.create);
 routes.get("/user", userController.get);
 routes.delete("/user/:id", userController.delete);
 routes.patch("/user/:id", userController.update);
+
+routes.post("/task", taskController.create);
+routes.get("/task", taskController.get);
+routes.delete("/task/:id", taskController.delete);
+routes.patch("/task/:id", taskController.update);
+
+routes.get("/", (request, response) => {
+    return response.send("Hello, world!");
+});
 
 export default routes;
